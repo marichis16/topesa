a9bf019b463ac837e5188a0a33d351ca2a78a170
marichis16 <marimarcillod@gmail.com>
2018-09-02 12:46:15 -0500

    login que funciona 1

--- angular_puce/login.js
+++ angular_puce/login.js
@@ -1,37 +1,30 @@
+window.onload = inicializar;
 
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-    var uiConfig = {
-        callbacks: {
-          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-            // User successfully signed in.
-            // Return type determines whether we continue the redirect automatically
-            // or whether we leave that to developer to handle.
-            return true;
-          },
-          uiShown: function() {
-            // The widget is rendered.
-            // Hide the loader.
-            document.getElementById('loader').style.display = 'none';
-          }
-        },
-        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-        signInFlow: 'popup',
-        signInSuccessUrl: './pantallaEmpleados.html',
-        signInOptions: [
-          // Leave the lines as is for the providers you want to offer your users.
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-          firebase.auth.GithubAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          firebase.auth.PhoneAuthProvider.PROVIDER_ID
-        ],
-        // Terms of service url.
-        tosUrl: './pantallaEmpleados.html',
-        // Privacy policy url.
-        //privacyPolicyUrl: '<your-privacy-policy-url>'
-      };
-      
-      ui.start('#firebaseui-auth-container', uiConfig);
+var form;
 
+function inicializar(){
+  form= document.getElementById("formulario");
+  form.addEventListener("submit", autenticacion, false);
+}
 
+function autenticacion(event){
+ 
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(result){
+      window.location.href="views/pantallaEmpleados.html";
+    })
+    .catch(function(error) {
+      alert("El nombre de usuario y la contraseña que ingresaste no coinciden con nuestros registros. Por favor, revisa e inténtalo de nuevo.");
+    });
+}
+
+// function crearNuevo(){
+//   event.preventDefault();
+//   var email= event.target.email.value;
+//   var password= event.target.password.value;
+
+//   firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+
+//   });
+// }
