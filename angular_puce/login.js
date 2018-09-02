b4fc0944c5cc79ef2db245d835c648ddd74f98a3
marichis16 <marimarcillod@gmail.com>
2018-09-02 13:16:39 -0500

    login mejorado que funciona

--- angular_puce/login.js
+++ angular_puce/login.js
@@ -1,30 +1,54 @@
-window.onload = inicializar;
+(function(){
 
-var form;
+    const config = {
+        apiKey: "AIzaSyD84c5uXptNfZa0UcaxvTuVZd2R3eTzvxA",
+        authDomain: "control-7c5d7.firebaseapp.com",
+        databaseURL: "https://control-7c5d7.firebaseio.com",
+        projectId: "control-7c5d7",
+        storageBucket: "control-7c5d7.appspot.com",
+        messagingSenderId: "635319972706"
+    };
+    firebase.initializeApp(config);
 
-function inicializar(){
-  form= document.getElementById("formulario");
-  form.addEventListener("submit", autenticacion, false);
-}
+    const txtEmail = document.getElementById("email");
+    const txtPassword= document.getElementById("password");
+    const btnLogin = document.getElementById("btnLogin");
+    const btnSignUp = document.getElementById("btnSignup");
 
-function autenticacion(event){
- 
+    btnLogin.addEventListener('click', e=> {
+        const email= txtEmail.value;
+        const password = txtPassword.value;
+        const auth= firebase.auth();
 
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(function(result){
-      window.location.href="views/pantallaEmpleados.html";
-    })
-    .catch(function(error) {
-      alert("El nombre de usuario y la contraseña que ingresaste no coinciden con nuestros registros. Por favor, revisa e inténtalo de nuevo.");
+        auth.signInWithEmailAndPassword(email, password)
+        .then(function(result){
+            window.location.href="views/pantallaEmpleados.html";
+        })
+        .catch(function(error) {
+            alert("El nombre de usuario y la contraseña que ingresaste no coinciden con nuestros registros. Por favor, revisa e inténtalo de nuevo.");
+        });
     });
-}
 
-// function crearNuevo(){
-//   event.preventDefault();
-//   var email= event.target.email.value;
-//   var password= event.target.password.value;
+    btnSignUp.addEventListener('click', e=>{
+        const email= txtEmail.value;
+        const password = txtPassword.value;
+        const auth= firebase.auth();
 
-//   firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        auth.createUserWithEmailAndPassword(email, password)
+        .catch(function(error) {
+            alert(error.message);
+            console.log(error.message);
+          });
+    });
 
-//   });
-// }
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if(firebaseUser){
+            console.log(firebaseUser);
+        }
+        else{
+            console.log('Error en autenticacion');
+        }
+           
+    });
+
+}());
