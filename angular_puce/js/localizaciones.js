265aee00e7f542ac23678de7152d5143291effea
Edison Gudiño <egudino484@puce.edu.ec>
2018-09-11 06:46:48 -0500

    localizaciones

---
+++ angular_puce/js/localizaciones.js
@@ -0,0 +1,185 @@
+var returnArr = [];
+
+  // Initialize Firebase
+
+  var config = {
+    apiKey: "AIzaSyD84c5uXptNfZa0UcaxvTuVZd2R3eTzvxA",
+    authDomain: "control-7c5d7.firebaseapp.com",
+    databaseURL: "https://control-7c5d7.firebaseio.com",
+    projectId: "control-7c5d7",
+    storageBucket: "control-7c5d7.appspot.com",
+    messagingSenderId: "635319972706"
+  };
+  firebase.initializeApp(config);
+
+
+    const lista = document.getElementById('lista');
+    const dbRef = firebase.database().ref().child('localizaciones');
+    //dbRef.on('value', snap => holamundo.innerText = snap.val());
+    //console.log(snap.val());
+   // const dbRefLista= dbRef.child('clientes');
+    dbRef.on('value', snap => {
+  returnArr=[];
+  snap.forEach(function(snap) {
+    console.log('Elemento');
+    var item=snap.val();
+    item.key = snap.key;
+
+     console.log( item);
+     returnArr.push(item);
+
+
+
+  });
+
+
+
+  console.log('Arreglo');
+
+  console.log( returnArr);
+
+
+
+
+
+//--------------------------------
+var table= document.getElementById('myTable');
+
+var tableHeaderRowCount = 1;
+var table = document.getElementById('myTable');
+var rowCount = table.rows.length;
+for (var i = tableHeaderRowCount; i < rowCount; i++) {
+    table.deleteRow(tableHeaderRowCount);
+}
+
+
+  for (i in returnArr) {
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+
+
+/*
+
+*/
+    cell1.innerHTML = returnArr.length-i;
+    cell2.innerHTML = returnArr[i].lat;
+    cell3.innerHTML = returnArr[i].lng;
+    cell4.innerHTML = returnArr[i].power;
+    cell5.innerHTML = returnArr[i].time;
+   
+}
+
+
+
+ /*const li= document.createElement('li');
+listaUsuarios=JSON.stringify(snap.val());
+
+ li.innerText = listaUsuarios;
+ lista.appendChild(li);*/
+
+  //console.log(JSON.stringify(snap.val()));
+
+
+});
+
+
+function myFunction() {
+var table= document.getElementById('myTable');
+
+var elmtTable = document.getElementById('myTable');
+var tableRows = elmtTable.getElementsByTagName('tr');
+var rowCount = tableRows.length;
+
+for (var x=rowCount-1; x>0; x--) {
+   elmtTable.removeChild(tableRows[x]);
+}
+
+  for (i in returnArr) {
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(6);
+    var cell8 = row.insertCell(7);
+
+    cell1.innerHTML = i;
+    cell2.innerHTML = returnArr[i].cedula;
+    cell3.innerHTML = returnArr[i].nombre;
+    cell4.innerHTML = returnArr[i].codigocuenta;
+    cell5.innerHTML = returnArr[i].correo;
+    cell6.innerHTML = returnArr[i].fecha;
+    cell7.innerHTML = returnArr[i].activa;
+    cell8.innerHTML = returnArr[i].enuso;
+
+
+
+  //  console.log('Cedula:'+ returnArr[i].cedula);
+
+}
+
+
+}
+
+function insertar(cedula,clave,codigocuenta, correo, latitud, longitud, fecha, nombre , tiponegocio){
+  const dbRef = firebase.database().ref().child('tutienda');
+  //dbRef.on('value', snap => holamundo.innerText = snap.val());
+  //console.log(snap.val());
+  const dbRefLista= dbRef.child('cuenta');
+var refnuevo = dbRefLista.push({
+  activa: true,
+  cedula: cedula,
+  clave: clave,
+  codigocuenta: codigocuenta,
+  correo: correo,
+  direccionlatitud: latitud,
+  direccionlongitud: longitud,
+  enuso: true,
+  existencia: true,
+  fecha: fecha,
+  nombre: nombre,
+tiponegocio: tiponegocio
+
+});
+// ingresar en la autenticacion el usuario 
+console.log("Obtenuiendo el usuario ingresado a firebase: ");
+console.log(refnuevo);
+
+
+}
+
+
+function ejecutarInsert(){
+var cedula= document.getElementById('cedula').value;
+var clave= document.getElementById('clave').value;
+var codigocuenta = document.getElementById('cedula').value;
+var correo = document.getElementById('correo').value;
+var latitud = 0//document.getElementById('latitud').value;
+var longitud = 0;//document.getElementById('longitud').value;
+var fecha = document.getElementById('fecha').value;
+fecha= fechaDDMMAA(fecha);
+    
+    
+var nombre = document.getElementById('nombre').value;
+var tiponegocio = document.getElementById('tiponegocio').value;
+    
+
+    
+insertar(cedula,clave,codigocuenta, correo, latitud, longitud, fecha, nombre ,tiponegocio);
+
+}
+
+    function fechaDDMMAA(fechaAAMMDD){
+        //2018-01-01 --> 01-01-2018
+        var anio= fechaAAMMDD.substring(0,4);
+        var mes= fechaAAMMDD.substring(5,7);
+        var dia= fechaAAMMDD.substring(8,10);
+        return dia+"-"+mes+"-"+anio;
+        
+    }
